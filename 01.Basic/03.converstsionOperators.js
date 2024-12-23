a3331f71a508a3d75d0f6c5ef02bdaf050bbeb17
Rahul Raj <86255032+KVSRahulraj@users.noreply.github.com>
2024-12-23 22:43:30 +0000

    Conversions and Opreators

---
+++ 01.Basic/03.converstsionOperators.js
@@ -0,0 +1,41 @@
+let score = 92
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let Name = "Rahul"  
+// let Name = "23afd" // NaN- Not a Number
+// let Name = true  // true => 1 ; false =>0
+
+
+let numberName = Number(Name)
+console.log(typeof Name);
+console.log(numberName);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let someNumber = 34
+
+let stringsomeNumber = String(someNumber)
+console.log(typeof stringsomeNumber);
+
+// "33" => 33
+// "33abc" => NaN
+
+
+let isLoggedin = "Rahul"
+
+let boolean = Boolean(isLoggedin)
+console.log(boolean);
+console.log(typeof boolean);
+
+//  true => 1; false => 0
+// "Rahul" => true
+// "" => false 
+
+/* *********************** Operations ***********************
+*/
+
